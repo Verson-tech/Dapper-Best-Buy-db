31ae5c46500a8f240c1a52107c744e9d3c1d23d3
Vira Murray <vira.avramenko@gmail.com>
2022-10-21 10:53:11 -0400

    Exercise 1

---
+++ Dapper-Best-Buy-db/IDepartmentRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper_Best_Buy_db
+{
+    internal interface IDepartmentRepository
+    {
+    }
+}
